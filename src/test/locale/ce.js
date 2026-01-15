f9d987bcfc0336fd481a613f7adefbf202ebea9e
TakhirgeranUmar <takhirgeran.umar@gmail.com>
2026-01-15 06:40:14 +0300

    Update ce.js

--- src/test/locale/ce.js
+++ src/test/locale/ce.js
@@ -10,7 +10,8 @@ test('format', function (assert) {
         ['LL', '2026 ш. 14 январь'],
         ['L', '2026.14.01'],
         ['LT', '15:25'],
-        ['MMMM', 'январь']
+        ['MMMM', 'январь'],
+        ['dddd', 'кхаара']
     ];
     var i;
     for (i = 0; i < a.length; i++) {
@@ -19,20 +20,30 @@ test('format', function (assert) {
 });
 
 test('calendar', function (assert) {
-    assert.equal(moment().subtract({ d: 1 }).calendar().split(' ')[0], 'Селхана', 'yesterday');
-    assert.equal(moment().calendar().split(' ')[0], 'Тахана', 'today');
-    assert.equal(moment().add({ d: 1 }).calendar().split(' ')[0], 'Кхана', 'tomorrow');
+    var now = moment([2026, 0, 14]); // Среда
+    assert.equal(moment(now).subtract({ d: 1 }).calendar().split(' ')[0], 'Селхана', 'yesterday');
+    assert.equal(moment(now).calendar().split(' ')[0], 'Тахана', 'today');
+    assert.equal(moment(now).add({ d: 1 }).calendar().split(' ')[0], 'Кхана', 'tomorrow');
+    
+    // Проверка Хьалхара / РогӀера
+    assert.equal(moment(now).subtract({ d: 7 }).calendar().split(' ')[0], '[Хьалхарчу]', 'last week');
+    assert.equal(moment(now).add({ d: 7 }).calendar().split(' ')[0], '[РогӀерчу]', 'next week');
 });
 
 test('relative time', function (assert) {
     assert.equal(moment.duration({ m: 1 }).humanize(), 'минот', 'a minute');
     assert.equal(moment.duration({ m: 5 }).humanize(), '5 минот', '5 minutes');
     assert.equal(moment.duration({ d: 1 }).humanize(), 'де', 'a day');
+    assert.equal(moment.duration({ d: 5 }).humanize(), '5 де', '5 days');
+    
+    // Проверка суффиксов (хьалха / тӀаьхьа)
+    assert.equal(moment([2026, 0, 14]).from(moment([2026, 0, 14, 0, 5])), '5 минот хьалха', '5 minutes ago');
+    assert.equal(moment([2026, 0, 14, 0, 5]).from(moment([2026, 0, 14])), '5 минот тӀаьхьа', 'in 5 minutes');
 });
 
 test('meridiem', function (assert) {
-    assert.equal(moment([2026, 0, 14, 2]).format('A'), 'буьйсана', 'night');
-    assert.equal(moment([2026, 0, 14, 8]).format('A'), 'Ӏуьйранна', 'morning');
-    assert.equal(moment([2026, 0, 14, 15]).format('A'), 'дийнахь', 'afternoon');
-    assert.equal(moment([2026, 0, 14, 21]).format('A'), 'суьйранна', 'evening');
+    assert.equal(moment([2026, 0, 14, 2]).format('A'), 'буьйса', 'night');
+    assert.equal(moment([2026, 0, 14, 8]).format('A'), 'Ӏуьйре', 'morning');
+    assert.equal(moment([2026, 0, 14, 15]).format('A'), 'де', 'afternoon');
+    assert.equal(moment([2026, 0, 14, 21]).format('A'), 'суьйре', 'evening');
 });
